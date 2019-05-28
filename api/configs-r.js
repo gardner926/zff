0efb6b0cd2e2d5acb081613a46e366fd515bee67
gardner926 <51129282+gardner926@users.noreply.github.com>
2019-05-29 00:08:45 +0800

    Create configs-r.js

---
+++ api/configs-r.js
@@ -0,0 +1,10 @@
+{
+    "host" : "http://pay.zhongfeipay.com/",
+    "version" : "RELEASE-ZF-4.0.0",
+    "about" : "https://github.com/huyquan/pmt",
+    "tryNotifyInterval" : 5000,
+    "tryCreateQRCodeInterval" : 5000,
+    "pushOnlineInterval" : 5000,
+    "remarkLength" : 15,
+    "remarkPrefix" : "KDF"
+}
